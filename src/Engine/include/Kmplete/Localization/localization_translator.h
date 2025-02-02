3c033cdc706da4eb51763823af003ff064f041e8
Ilya Malgin <Soundmaker1505@gmail.com>
2025-02-02 17:36:11 +0300

    localization translator added

---
+++ src/Engine/include/Kmplete/Localization/localization_translator.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Kmplete/Localization/localization_base.h"
+#include "Kmplete/Core/kmplete_api.h"
+
+#include <boost/locale.hpp>
+
+#include <string>
+
+namespace Kmplete
+{
+    namespace Translator
+    {
+        KMP_NODISCARD KMP_API TranslationStr Translate(const DomainStr& domain, const SourceStr& source);
+        KMP_NODISCARD KMP_API TranslationStr Translate(const DomainStr& domain, const SourceStr& sourceSingular, const SourceStr& sourcePlural, int count);
+        KMP_NODISCARD KMP_API TranslationStr TranslateCtx(const DomainStr& domain, const SourceStr& source, const ContextStr& context);
+        KMP_NODISCARD KMP_API TranslationStr TranslateCtx(const DomainStr& domain, const SourceStr& sourceSingular, const SourceStr& sourcePlural, int count, const ContextStr& context);
+
+        inline void TranslateDefer(const DomainStr&, const SourceStr&) {};
+        inline void TranslateDefer(const DomainStr&, const SourceStr&, const SourceStr&, int) {};
+        inline void TranslateCtxDefer(const DomainStr&, const SourceStr&, const ContextStr&) {};
+        inline void TranslateCtxDefer(const DomainStr&, const SourceStr&, const SourceStr&, int, const ContextStr&) {};
+
+        template<typename... Types>
+        KMP_NODISCARD TranslationStr Format(const SourceStr& source, Types&&... args)
+        {
+            return (boost::locale::format(source) % ... % std::forward<Types>(args)).str();
+        }
+        //--------------------------------------------------------------------------
+    }
+}
---
+++ src/Engine/src/Localization/localization_translator.cpp
@@ -0,0 +1,31 @@
+#include "Kmplete/Localization/localization_translator.h"
+
+namespace Kmplete
+{
+    namespace Translator
+    {
+        TranslationStr Translate(const DomainStr& domain, const SourceStr& source)
+        {
+            return boost::locale::translate(source).str(domain);
+        }
+        //--------------------------------------------------------------------------
+
+        TranslationStr Translate(const DomainStr& domain, const SourceStr& sourceSingular, const SourceStr& sourcePlural, int count)
+        {
+            return boost::locale::translate(sourceSingular, sourcePlural, count).str(domain);
+        }
+        //--------------------------------------------------------------------------
+
+        TranslationStr TranslateCtx(const DomainStr& domain, const SourceStr& source, const ContextStr& context)
+        {
+            return boost::locale::translate(context, source).str(domain);
+        }
+        //--------------------------------------------------------------------------
+
+        TranslationStr TranslateCtx(const DomainStr& domain, const SourceStr& sourceSingular, const SourceStr& sourcePlural, int count, const ContextStr& context)
+        {
+            return boost::locale::translate(context, sourceSingular, sourcePlural, count).str(domain);
+        }
+        //--------------------------------------------------------------------------
+    }
+}
