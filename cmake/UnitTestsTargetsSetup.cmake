1a59f0eb25ae9c615a204377302df4f9a038a307
Ilya Malgin <Soundmaker1505@gmail.com>
2025-06-21 15:34:49 +0300

    UnitTests targets use ARGN as link_libraries list

--- cmake/UnitTestsTargetsSetup.cmake
+++ cmake/UnitTestsTargetsSetup.cmake
@@ -12,6 +12,10 @@ function(SetupUnitTestsTargetProperties target folder)
             VS_DEBUGGER_WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/bin/$<CONFIG>
         )
     endif()
+    
+    target_link_libraries(${target}
+        PRIVATE ${ARGN}
+    )
 
     include(Catch)
     catch_discover_tests(${target})
