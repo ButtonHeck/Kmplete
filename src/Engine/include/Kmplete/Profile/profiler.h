520b2d6e83f480aa0a98d3a42f490af070fdcd6b
Ilya Malgin <Soundmaker1505@gmail.com>
2025-06-29 16:39:20 +0300

    KMP_FUNC_SIG moved from Profiler to UtilsLib

--- src/Engine/include/Kmplete/Profile/profiler.h
+++ src/Engine/include/Kmplete/Profile/profiler.h
@@ -4,6 +4,7 @@
 #include "Kmplete/Base/pointers.h"
 #include "Kmplete/Base/kmplete_api.h"
 #include "Kmplete/Core/timer.h"
+#include "Kmplete/Utils/function_utils.h"
 
 #include <chrono>
 #include <thread>
@@ -68,26 +69,8 @@ namespace Kmplete
     //--------------------------------------------------------------------------
 }
 
-#define KMP_PROFILE 1
+#define KMP_PROFILE true
 #if KMP_PROFILE && !defined (KMP_PRODUCTION_BUILD)
-    #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
-        #define KMP_FUNC_SIG __PRETTY_FUNCTION__
-    #elif defined(__DMC__) && (__DMC__ >= 0x810)
-        #define KMP_FUNC_SIG __PRETTY_FUNCTION__
-    #elif (defined(__FUNCSIG__) || (_MSC_VER))
-        #define KMP_FUNC_SIG __FUNCSIG__
-    #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
-        #define KMP_FUNC_SIG __FUNCTION__
-    #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
-        #define KMP_FUNC_SIG __FUNC__
-    #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
-        #define KMP_FUNC_SIG __func__
-    #elif defined(__cplusplus) && (__cplusplus >= 201103)
-        #define KMP_FUNC_SIG __func__
-    #else
-        #define KMP_FUNC_SIG "KMP_FUNC_SIG unknown!"
-    #endif
-
     #define KMP_PROFILE_BEGIN_SESSION(name, filepath) ::Kmplete::Profiler::Get().BeginSession(name, filepath)
     #define KMP_PROFILE_END_SESSION() ::Kmplete::Profiler::Get().EndSession()
     #define KMP_PROFILE_SCOPE_LINE2(name, line) ::Kmplete::ProfilerTimer timer##line(name)
