46b32c23424f7cfde478468bd20cde244d26a07a
Ilya Malgin <soundmaker1505@gmail.com>
2025-09-16 11:48:05 +0300

    JsonLib cyrillic path fixes

--- src/JsonLib/tests/json_document_tests.cpp
+++ src/JsonLib/tests/json_document_tests.cpp
@@ -401,9 +401,17 @@ TEST_CASE("Json document save/load with cyrillic path", "[json][reader][writer][
     auto localizationManager = Kmplete::LocalizationManager();
     localizationManager.SetLocale("ru_RU.UTF8");
 
+    auto settingsFilepath = Kmplete::Filesystem::GetCurrentFilepath();
     const auto settingsSubPath = Kmplete::Utils::Utf8ToNarrow("Тест");
+
+#if defined (KMP_PLATFORM_WINDOWS)
     const auto settingsSubPathWide = Kmplete::Utils::NarrowToWide(settingsSubPath);
-    const auto settingsFilepath = Kmplete::Filesystem::GetCurrentFilepath().append(settingsSubPathWide).append("json_document_test_temp_unescaped.json");
+    settingsFilepath.append(settingsSubPathWide);
+#else
+    settingsFilepath.append(settingsSubPath);
+#endif
+
+    settingsFilepath.append("json_document_test_temp_unescaped.json");
 
     Kmplete::JsonDocument rootDoc;
     REQUIRE(rootDoc.SetInt("AnInt", 13));
