fd65930a400096ecf76ac1ed9ab47055991e9491
Ilya Malgin <Soundmaker1505@gmail.com>
2025-10-10 13:13:50 +0300

    assets compiler constants moved to separate header

--- src/AssetsCompiler/assets_compiler.h
+++ src/AssetsCompiler/assets_compiler.h
@@ -3,6 +3,7 @@
 #include "Kmplete/Base/kmplete_api.h"
 #include "Kmplete/Base/types_aliases.h"
 #include "Kmplete/Assets/assets_interface.h"
+#include "Kmplete/Assets/assets_compiler_interface.h"
 #include "Kmplete/Log/log_class_macro.h"
 
 
---
+++ src/AssetsInterfaceLib/include/Kmplete/Assets/assets_compiler_interface.h
@@ -0,0 +1,40 @@
+#pragma once
+
+
+namespace Kmplete
+{
+    namespace Assets
+    {
+        namespace Compiler
+        {
+            static constexpr auto JsonConfigurationTopLevelName = "Assets";
+            static constexpr auto JsonConfigurationFileStr = "File";
+            static constexpr auto JsonConfigurationTypeStr = "Type";
+            static constexpr auto JsonConfigurationSidStr = "Sid";
+
+            static constexpr auto CompilerArgumentLogging = "logging";
+            static constexpr auto CompilerArgumentLoggingShort = "L";
+            static constexpr auto CompilerArgumentInputFileName = "input_file_name";
+            static constexpr auto CompilerArgumentInputFileNameShort = "I";
+            static constexpr auto CompilerArgumentOutputFileName = "output_file_name";
+            static constexpr auto CompilerArgumentOutputFileNameShort = "O";
+
+            enum ReturnCode
+            {
+                Ok = 0,
+                InvalidArgumentCount = -1,
+
+                InputFileIsNotSet = -20,
+                InputFileIsNotValid = -21,
+                InputFileParsingError = -22,
+                InputFileFormatError = -23,
+                InputFileProcessingError = -24,
+
+                OutputFileIsNotSet = -30,
+                OutputFileCreationFailed = -31,
+                OutputFileOpeningFailed = -32
+            };
+        }
+        //--------------------------------------------------------------------------
+    }
+}
