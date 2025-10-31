870a2eb990144ca7a1293ac247da6994a3305327
Ilya Malgin <Soundmaker1505@gmail.com>
2025-10-31 08:51:40 +0300

    Production build fix

--- src/Engine/include/Kmplete/Graphics/font_manager.h
+++ src/Engine/include/Kmplete/Graphics/font_manager.h
@@ -2,6 +2,7 @@
 
 #include "Kmplete/Base/kmplete_api.h"
 #include "Kmplete/Base/types_aliases.h"
+#include "Kmplete/Base/pointers.h"
 #include "Kmplete/Graphics/font.h"
 #include "Kmplete/Utils/string_id.h"
 #include "Kmplete/Log/log_class_macro.h"
