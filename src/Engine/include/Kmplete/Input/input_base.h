0e7aa42725787e5a26e1798568f608e736f43827
Ilya Malgin <Soundmaker1505@gmail.com>
2025-12-21 12:17:48 +0300

    Input structures extracted to input_base.h header

---
+++ src/Engine/include/Kmplete/Input/input_base.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "Kmplete/Base/functional.h"
+#include "Kmplete/Base/string_id.h"
+#include "Kmplete/Input/input_codes.h"
+#include "Kmplete/Math/geometry.h"
+
+#include <variant>
+
+
+namespace Kmplete
+{
+    namespace Input
+    {
+        using InputControlValue = std::variant<int, float, Math::Point2I, std::monostate>;
+
+        static constexpr auto ButtonPressed = 1;
+        static constexpr auto ButtonReleased = 0;
+
+        static constexpr auto ButtonPressedValue = InputControlValue(ButtonPressed);
+        static constexpr auto ButtonReleasedValue = InputControlValue(ButtonReleased);
+        static constexpr auto EmptyValue = InputControlValue(std::monostate());
+
+        static constexpr auto InputControlValueIntIndex = 0;
+        static constexpr auto InputControlValueFloatIndex = 1;
+        static constexpr auto InputControlValuePointIndex = 2;
+
+
+        using ActionIdentifier = StringID;
+        using ActionCallbackTag = StringID;
+        using ActionCallback = Function<bool(InputControlValue)>;
+
+
+        struct TaggedActionCallback
+        {
+            ActionCallbackTag tag;
+            ActionCallback callback;
+        };
+        //--------------------------------------------------------------------------
+
+        static constexpr ActionCallbackTag DefaultActionCallbackTag = 0ULL;
+
+
+        struct ActionEvent
+        {
+            ActionIdentifier id;
+            InputControlValue value;
+        };
+        //--------------------------------------------------------------------------
+
+
+        struct InputCondition
+        {
+            InputControlValue value = EmptyValue;
+            KeyModifier modifierMask = Input::Modifier::None;
+        };
+        //--------------------------------------------------------------------------
+
+        static constexpr InputCondition PressNoModsCondition = InputCondition{ .value = ButtonPressedValue, .modifierMask = Input::Modifier::None };
+        static constexpr InputCondition NoCondition = InputCondition{};
+
+
+        struct InputCodeWithCondition
+        {
+            InputCode code;
+            InputCondition condition;
+        };
+        //--------------------------------------------------------------------------
+    }
+}
--- src/Engine/include/Kmplete/Input/input_manager.h
+++ src/Engine/include/Kmplete/Input/input_manager.h
@@ -2,14 +2,11 @@
 
 #include "Kmplete/Base/kmplete_api.h"
 #include "Kmplete/Base/types_aliases.h"
-#include "Kmplete/Base/string_id.h"
-#include "Kmplete/Base/functional.h"
+#include "Kmplete/Input/input_base.h"
 #include "Kmplete/Input/input_codes.h"
 #include "Kmplete/Math/geometry.h"
 #include "Kmplete/Log/log_class_macro.h"
 #include "Kmplete/Profile/profiler.h"
-
-#include <variant>
 
 
 namespace Kmplete
@@ -22,61 +19,6 @@ namespace Kmplete
 
     namespace Input
     {
-        using InputControlValue = std::variant<int, float, Math::Point2I, std::monostate>;
-
-        static constexpr auto ButtonPressed = 1;
-        static constexpr auto ButtonReleased = 0;
-
-        static constexpr auto ButtonPressedValue = InputControlValue(ButtonPressed);
-        static constexpr auto ButtonReleasedValue = InputControlValue(ButtonReleased);
-        static constexpr auto EmptyValue = InputControlValue(std::monostate());
-
-        static constexpr auto InputControlValueIntIndex = 0;
-        static constexpr auto InputControlValueFloatIndex = 1;
-        static constexpr auto InputControlValuePointIndex = 2;
-
-
-        using ActionIdentifier = StringID;
-        using ActionCallbackTag = StringID;
-        using ActionCallback = Function<bool(InputControlValue)>;
-
-
-        struct TaggedActionCallback
-        {
-            ActionCallbackTag tag;
-            ActionCallback callback;
-        };
-        //--------------------------------------------------------------------------
-
-        static constexpr ActionCallbackTag DefaultActionCallbackTag = 0ULL;
-
-
-        struct ActionEvent
-        {
-            ActionIdentifier id;
-            InputControlValue value;
-        };
-        //--------------------------------------------------------------------------
-
-
-        struct InputCondition
-        {
-            InputControlValue value = EmptyValue;
-            KeyModifier modifierMask = Input::Modifier::None;
-        };
-        //--------------------------------------------------------------------------
-
-        static constexpr InputCondition PressNoModsCondition = InputCondition{ .value = ButtonPressedValue, .modifierMask = Input::Modifier::None };
-        static constexpr InputCondition NoCondition = InputCondition{};
-
-        struct InputCodeWithCondition
-        {
-            InputCode code;
-            InputCondition condition;
-        };
-        //--------------------------------------------------------------------------
-
-
         class InputManager
         {
             KMP_LOG_CLASSNAME(InputManager)
