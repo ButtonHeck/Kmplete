ed8c76fee85bbe358d9d943815e6b9d7bd3f6b2e
Ilya Malgin <Soundmaker1505@gmail.com>
2026-01-24 18:58:10 +0300

    Top-level CMakeLists.txt minimum version raised to 3.24 for new Vulkan module

---
+++ cmake/Vulkan.cmake
@@ -0,0 +1,9 @@
+## ~/cmake/Vulkan.cmake
+
+############################################################################
+# CMake module for finding Vulkan and its components
+############################################################################
+
+find_package(Vulkan COMPONENTS
+    glslc
+)
