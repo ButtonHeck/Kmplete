4d451e979e217fd99e336ba7e33570a464038299
Ilya Malgin <Soundmaker1505@gmail.com>
2026-02-07 16:31:09 +0300

    VkResult description helper function added

---
+++ src/Libraries/Engine/include/Kmplete/Graphics/Vulkan/vulkan_result_description.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Kmplete/Base/kmplete_api.h"
+#include "Kmplete/Base/types_aliases.h"
+
+#include <vulkan/vulkan.h>
+
+
+namespace Kmplete
+{
+    namespace Graphics
+    {
+        KMP_NODISCARD KMP_API String VkResultToString(VkResult result);
+    }
+}
