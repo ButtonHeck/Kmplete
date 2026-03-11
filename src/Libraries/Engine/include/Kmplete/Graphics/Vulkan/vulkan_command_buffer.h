9ef80b334cb3407dc65d3130013e0f1ae0f63b39
Ilya Malgin <Soundmaker1505@gmail.com>
2026-03-11 16:35:42 +0300

    VulkanCommandBuffer class added

---
+++ src/Libraries/Engine/include/Kmplete/Graphics/Vulkan/vulkan_command_buffer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Kmplete/Base/kmplete_api.h"
+
+#include <vulkan/vulkan.h>
+
+
+namespace Kmplete
+{
+    namespace Graphics
+    {
+        class VulkanCommandBuffer
+        {
+        };
+        //--------------------------------------------------------------------------
+    }
+}
---
+++ src/Libraries/Engine/src/Graphics/Vulkan/vulkan_command_buffer.cpp
@@ -0,0 +1,10 @@
+#include "Kmplete/Graphics/Vulkan/vulkan_command_buffer.h"
+
+
+namespace Kmplete
+{
+    namespace Graphics
+    {
+
+    }
+}
