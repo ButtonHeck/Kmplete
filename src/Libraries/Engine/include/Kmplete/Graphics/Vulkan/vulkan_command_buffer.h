4dc3e48b1e459c797a42fab8836cb637ff3a6ff9
Ilya Malgin <Soundmaker1505@gmail.com>
2026-03-12 13:16:43 +0300

    VulkanCommandBuffer basic api added

--- src/Libraries/Engine/include/Kmplete/Graphics/Vulkan/vulkan_command_buffer.h
+++ src/Libraries/Engine/include/Kmplete/Graphics/Vulkan/vulkan_command_buffer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Kmplete/Base/kmplete_api.h"
+#include "Kmplete/Base/type_traits.h"
 
 #include <vulkan/vulkan.h>
 
@@ -11,7 +12,27 @@ namespace Kmplete
     {
         class VulkanCommandBuffer
         {
+            KMP_DISABLE_COPY(VulkanCommandBuffer)
+
+        public:
+            KMP_API VulkanCommandBuffer(VkDevice device, VkCommandPool commandPool);
+            KMP_API VulkanCommandBuffer(VulkanCommandBuffer&& other) noexcept;
+            KMP_API VulkanCommandBuffer& operator=(VulkanCommandBuffer&& other) noexcept;
+            KMP_API ~VulkanCommandBuffer();
+
+            KMP_API void Begin(VkCommandBufferUsageFlags flags = 0) const;
+            KMP_API void End() const;
+
+            KMP_API void Submit(VkQueue queue, VkFence fence = VK_NULL_HANDLE, bool waitFence = false) const;
+
+        private:
+            VkDevice _device;
+            VkCommandPool _commandPool;
+            VkCommandBuffer _commandBuffer;
         };
         //--------------------------------------------------------------------------
+
+        static_assert(IsMoveConstructible<VulkanCommandBuffer>::value);
+        static_assert(IsMoveAssignable<VulkanCommandBuffer>::value);
     }
 }
--- src/Libraries/Engine/src/Graphics/Vulkan/vulkan_command_buffer.cpp
+++ src/Libraries/Engine/src/Graphics/Vulkan/vulkan_command_buffer.cpp
@@ -1,10 +1,108 @@
 #include "Kmplete/Graphics/Vulkan/vulkan_command_buffer.h"
+#include "Kmplete/Graphics/Vulkan/Utils/initializers.h"
+#include "Kmplete/Graphics/Vulkan/Utils/result_description.h"
+#include "Kmplete/Profile/profiler.h"
 
 
 namespace Kmplete
 {
     namespace Graphics
     {
+        VulkanCommandBuffer::VulkanCommandBuffer(VkDevice device, VkCommandPool commandPool)
+            : _device(device)
+            , _commandPool(commandPool)
+            , _commandBuffer(VK_NULL_HANDLE)
+        {
+            KMP_PROFILE_FUNCTION(ProfileLevelAlways);
 
+            auto commandBufferAllocateInfo = VulkanUtils::InitVkCommandBufferAllocateInfo();
+            commandBufferAllocateInfo.commandPool = _commandPool;
+            commandBufferAllocateInfo.commandBufferCount = 1;
+
+            const auto result = vkAllocateCommandBuffers(_device, &commandBufferAllocateInfo, &_commandBuffer);
+            VulkanUtils::CheckResult(result, "VulkanCommandBuffer: failed to allocate command buffer");
+        }
+        //--------------------------------------------------------------------------
+
+        VulkanCommandBuffer::VulkanCommandBuffer(VulkanCommandBuffer&& other) noexcept
+            : _device(other._device)
+            , _commandPool(other._commandPool)
+            , _commandBuffer(other._commandBuffer)
+        {
+            other._device = VK_NULL_HANDLE;
+            other._commandPool = VK_NULL_HANDLE;
+            other._commandBuffer = VK_NULL_HANDLE;
+        }
+        //--------------------------------------------------------------------------
+
+        VulkanCommandBuffer& VulkanCommandBuffer::operator=(VulkanCommandBuffer&& other) noexcept
+        {
+            if (this == &other)
+            {
+                return *this;
+            }
+
+            _device = other._device;
+            _commandPool = other._commandPool;
+            _commandBuffer = other._commandBuffer;
+
+            other._device = VK_NULL_HANDLE;
+            other._commandPool = VK_NULL_HANDLE;
+            other._commandBuffer = VK_NULL_HANDLE;
+
+            return *this;
+        }
+        //--------------------------------------------------------------------------
+
+        VulkanCommandBuffer::~VulkanCommandBuffer()
+        {
+            KMP_PROFILE_FUNCTION(ProfileLevelAlways);
+
+            if (_device && _commandPool && _commandBuffer)
+            {
+                vkFreeCommandBuffers(_device, _commandPool, 1, &_commandBuffer);
+            }
+        }
+        //--------------------------------------------------------------------------
+
+        void VulkanCommandBuffer::Begin(VkCommandBufferUsageFlags flags /*= 0*/) const
+        {
+            KMP_PROFILE_FUNCTION(ProfileLevelMinorFunctions);
+
+            auto commandBufferBeginInfo = VulkanUtils::InitVkCommandBufferBeginInfo();
+            commandBufferBeginInfo.flags |= flags;
+
+            const auto result = vkBeginCommandBuffer(_commandBuffer, &commandBufferBeginInfo);
+            VulkanUtils::CheckResult(result, "VulkanCommandBuffer: failed to begin command buffer");
+        }
+        //--------------------------------------------------------------------------
+
+        void VulkanCommandBuffer::End() const
+        {
+            KMP_PROFILE_FUNCTION(ProfileLevelMinorFunctions);
+
+            const auto result = vkEndCommandBuffer(_commandBuffer);
+            VulkanUtils::CheckResult(result, "VulkanCommandBuffer: failed to end command buffer");
+        }
+        //--------------------------------------------------------------------------
+
+        void VulkanCommandBuffer::Submit(VkQueue queue, VkFence fence /*= VK_NULL_HANDLE*/, bool waitFence /*= false*/) const
+        {
+            KMP_PROFILE_FUNCTION(ProfileLevelMinorFunctions);
+
+            auto submitInfo = VulkanUtils::InitVkSubmitInfo();
+            submitInfo.commandBufferCount = 1;
+            submitInfo.pCommandBuffers = &_commandBuffer;
+
+            auto result = vkQueueSubmit(queue, 1, &submitInfo, fence);
+            VulkanUtils::CheckResult(result, "VulkanCommandBuffer: failed to submit to queue");
+
+            if (fence != VK_NULL_HANDLE && waitFence)
+            {
+                result = vkWaitForFences(_device, 1, &fence, VK_TRUE, UINT64_MAX);
+                Graphics::VulkanUtils::CheckResult(result, "VulkanCommandBuffer: failed to wait on fence");
+            }
+        }
+        //--------------------------------------------------------------------------
     }
 }
