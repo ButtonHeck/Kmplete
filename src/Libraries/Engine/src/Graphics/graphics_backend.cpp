91b21c542387e2c4c86ed0362556b24d23ebde1f
Ilya Malgin <Soundmaker1505@gmail.com>
2026-04-28 11:42:47 +0300

    GraphicsBackend Create log level changed to error

--- src/Libraries/Engine/src/Graphics/graphics_backend.cpp
+++ src/Libraries/Engine/src/Graphics/graphics_backend.cpp
@@ -20,7 +20,7 @@ namespace Kmplete
             case GraphicsBackendType::Vulkan:
                 return CreateUPtr<VulkanGraphicsBackend>(window);
             default:
-                KMP_LOG_CRITICAL("cannot create graphics backend instance for graphics backend '{}'", GraphicsBackendTypeToString(graphicsBackendType));
+                KMP_LOG_ERROR("cannot create graphics backend instance for graphics backend '{}'", GraphicsBackendTypeToString(graphicsBackendType));
                 return nullptr;
             }
         }}
