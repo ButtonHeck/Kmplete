4930457286e088f4dece5fb70bc85cd6799135d9
Ilya Malgin <Soundmaker1505@gmail.com>
2026-06-11 08:48:04 +0300

    ProfilerLib add fwd header for use in class declarations

--- src/Libraries/ProfilerLib/include/Kmplete/Profile/profiler.h
+++ src/Libraries/ProfilerLib/include/Kmplete/Profile/profiler.h
@@ -1,15 +1,11 @@
 #pragma once
 
-//! Helper macro definition to manually turn on/off profiling in the code (static builds only)
-//! for Debug and Release builds
-#define KMP_PROFILE_MANUAL_SET true
+#include "Kmplete/Profile/profiler_fwd.h"
 
-#if ((!defined KMP_BUILD_STATIC || KMP_PROFILE_MANUAL_SET) && !defined (KMP_CONFIG_TYPE_PRODUCTION)) || defined (KMP_CONFIG_TYPE_RELWITHDEBINFO)
-#define KMP_PROFILE
+#if defined(KMP_PROFILE)
 
 #include "Kmplete/Base/types_aliases.h"
 #include "Kmplete/Base/type_traits.h"
-#include "Kmplete/Base/pointers.h"
 #include "Kmplete/Base/kmplete_api.h"
 #include "Kmplete/Base/macro.h"
 #include "Kmplete/Log/log_class_macro.h"
@@ -118,7 +114,7 @@ namespace Kmplete
         KMP_DISABLE_COPY_MOVE(ProfilerTimer)
 
     public:
-        KMP_API explicit ProfilerTimer(const char* name, unsigned int level = 0);
+        KMP_API explicit ProfilerTimer(const char* name, unsigned int level = ProfileLevelAlways);
         KMP_API ~ProfilerTimer();
 
         KMP_API void SetName(const char* name);
@@ -208,11 +204,8 @@ namespace Kmplete
 
 #define KMP_PROFILING(level) { KMP_PROFILE_FUNCTION(level);
 
-//! Shortcut macros for profiling objects' constructors including members initializer lists stage
-#define KMP_PROFILE_CONSTRUCTOR_DECLARE() \
-    private:\
-    UPtr<ProfilerTimer> _constructorProfilerTimer;
 
+//! Shortcut macros for profiling objects' constructors including members initializer lists stage
 #define KMP_PROFILE_CONSTRUCTOR_START_BASE_CLASS() \
     _constructorProfilerTimer(CreateUPtr<ProfilerTimer>("")) ,
 
@@ -257,22 +250,3 @@ namespace Kmplete
 #define KMP_PROFILE_CONSTRUCTOR_START_DERIVED_CLASS()
 #define KMP_PROFILE_CONSTRUCTOR_END()
 #endif
-
-
-namespace Kmplete
-{
-    //! Enumeration of the profiling levels from the most important (0) to the least (4).
-    //! There is no strict rule which level in which function should be used, but the common
-    //! rule in this project is to use ProfileLevelAlways in constructors/destructors and functions
-    //! like Initialize/Finalize, ProfileLevelImportant(Verbose) for somewhat "heavy" and
-    //! important functions, and ProfileLevelMinor(Verbose) for any other functions
-    enum ProfileLevel : unsigned int
-    {
-        ProfileLevelAlways = 0,
-        ProfileLevelImportant = 1,
-        ProfileLevelImportantVerbose = 2,
-        ProfileLevelMinor = 3,
-        ProfileLevelMinorVerbose = 4
-    };
-    //--------------------------------------------------------------------------
-}
---
+++ src/Libraries/ProfilerLib/include/Kmplete/Profile/profiler_fwd.h
@@ -0,0 +1,41 @@
+#pragma once
+
+//! Helper macro definition to manually turn on/off profiling in the code (static builds only)
+//! for Debug and Release builds
+#define KMP_PROFILE_MANUAL_SET true
+
+#if ((!defined KMP_BUILD_STATIC || KMP_PROFILE_MANUAL_SET) && !defined (KMP_CONFIG_TYPE_PRODUCTION)) || defined (KMP_CONFIG_TYPE_RELWITHDEBINFO)
+#define KMP_PROFILE
+
+#include "Kmplete/Base/pointers.h"
+
+namespace Kmplete
+{
+    class ProfilerTimer;
+}
+
+#define KMP_PROFILE_CONSTRUCTOR_DECLARE() \
+    private:\
+    UPtr<ProfilerTimer> _constructorProfilerTimer;
+#else
+#define KMP_PROFILE_CONSTRUCTOR_DECLARE()
+#endif
+
+
+namespace Kmplete
+{
+    //! Enumeration of the profiling levels from the most important (0) to the least (4).
+    //! There is no strict rule which level in which function should be used, but the common
+    //! rule in this project is to use ProfileLevelAlways in constructors/destructors and functions
+    //! like Initialize/Finalize, ProfileLevelImportant(Verbose) for somewhat "heavy" and
+    //! important functions, and ProfileLevelMinor(Verbose) for any other functions
+    enum ProfileLevel : unsigned int
+    {
+        ProfileLevelAlways = 0,
+        ProfileLevelImportant = 1,
+        ProfileLevelImportantVerbose = 2,
+        ProfileLevelMinor = 3,
+        ProfileLevelMinorVerbose = 4
+    };
+    //--------------------------------------------------------------------------
+}
