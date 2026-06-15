98c12366bae80e0a51ea68b0a98c9c3797ca6de0
Ilya Malgin <Soundmaker1505@gmail.com>
2026-06-15 13:17:24 +0300

    VulkanBufferManager add functions for createget buffers

--- src/Libraries/Engine/include/Kmplete/Graphics/Vulkan/Buffer/vulkan_buffer_manager.h
+++ src/Libraries/Engine/include/Kmplete/Graphics/Vulkan/Buffer/vulkan_buffer_manager.h
@@ -4,7 +4,12 @@
 #include "Kmplete/Base/types_aliases.h"
 #include "Kmplete/Base/string_id.h"
 #include "Kmplete/Base/pointers.h"
+#include "Kmplete/Base/nullability.h"
+#include "Kmplete/Graphics/graphics_base.h"
+#include "Kmplete/Graphics/Vulkan/Buffer/vulkan_buffer.h"
+#include "Kmplete/Graphics/Vulkan/Buffer/vulkan_vertex_buffer.h"
 #include "Kmplete/Profile/profiler_fwd.h"
+#include "Kmplete/Log/log_class_macro.h"
 
 #include <vulkan/vulkan.h>
 
@@ -20,15 +25,44 @@ namespace Kmplete
         class VulkanBufferManager
         {
             KMP_DISABLE_COPY_MOVE(VulkanBufferManager)
+            KMP_LOG_CLASSNAME(VulkanBufferManager)
             KMP_PROFILE_CONSTRUCTOR_DECLARE()
 
         public:
             KMP_API VulkanBufferManager(VkDevice device, const VulkanBufferCreatorDelegate& bufferCreatorDelegate);
             ~VulkanBufferManager() = default;
 
+            KMP_NODISCARD KMP_API VulkanBuffer CreateBuffer(const VulkanBufferParameters& parameters) const;
+            KMP_API bool CreateBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame = false);
+
+            KMP_NODISCARD KMP_API VulkanVertexBuffer CreateVertexBuffer(const VulkanBufferParameters& parameters) const;
+            KMP_API bool CreateVertexBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame = false);
+
+            KMP_NODISCARD KMP_API VulkanBuffer CreateIndexBuffer(const VulkanBufferParameters& parameters) const;
+            KMP_API bool CreateIndexBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame = false);
+
+            KMP_NODISCARD KMP_API VulkanBuffer CreateUniformBuffer(const VulkanBufferParameters& parameters) const;
+            KMP_API bool CreateUniformBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame = false);
+
+            KMP_NODISCARD KMP_API VulkanBuffer CreateStorageBuffer(const VulkanBufferParameters& parameters) const;
+            KMP_API bool CreateStorageBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame = false);
+
+            KMP_NODISCARD KMP_API VulkanBuffer CreateIndirectBuffer(const VulkanBufferParameters& parameters) const;
+            KMP_API bool CreateIndirectBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame = false);
+
+            KMP_NODISCARD KMP_API Nullable<VulkanBuffer*> GetBuffer(StringID bufferSid) const noexcept;
+            KMP_NODISCARD KMP_API Nullable<VulkanBuffer*> GetBuffer(StringID bufferSid, UInt32 index) const noexcept;
+            KMP_NODISCARD KMP_API Nullable<VulkanVertexBuffer*> GetVertexBuffer(StringID bufferSid) const noexcept;
+            KMP_NODISCARD KMP_API Nullable<VulkanVertexBuffer*> GetVertexBuffer(StringID bufferSid, UInt32 index) const noexcept;
+
         private:
             VkDevice _device;
             const VulkanBufferCreatorDelegate& _bufferCreatorDelegate;
+
+            StringIDHashMap<UPtr<VulkanBuffer>> _buffers;
+            StringIDHashMap<UPtr<VulkanVertexBuffer>> _vertexBuffers;
+            StringIDHashMap<Array<UPtr<VulkanBuffer>, NumConcurrentFrames>> _perFrameBuffers;
+            StringIDHashMap<Array<UPtr<VulkanVertexBuffer>, NumConcurrentFrames>> _perFrameVertexBuffers;
         };
         //--------------------------------------------------------------------------
     }
--- src/Libraries/Engine/src/Graphics/Vulkan/Buffer/vulkan_buffer_manager.cpp
+++ src/Libraries/Engine/src/Graphics/Vulkan/Buffer/vulkan_buffer_manager.cpp
@@ -2,6 +2,7 @@
 #include "Kmplete/Graphics/Vulkan/Delegates/vulkan_buffer_creator_delegate.h"
 #include "Kmplete/Core/assertion.h"
 #include "Kmplete/Profile/profiler.h"
+#include "Kmplete/Log/log.h"
 
 
 namespace Kmplete
@@ -12,9 +13,344 @@ namespace Kmplete
             : KMP_PROFILE_CONSTRUCTOR_START_BASE_CLASS()
               _device(device)
             , _bufferCreatorDelegate(bufferCreatorDelegate)
+            , _buffers()
+            , _vertexBuffers()
+            , _perFrameBuffers()
+            , _perFrameVertexBuffers()
         {
             KMP_ASSERT(_device);
             KMP_PROFILE_CONSTRUCTOR_END()
+        }
+        //--------------------------------------------------------------------------
+
+
+        VulkanBuffer VulkanBufferManager::CreateBuffer(const VulkanBufferParameters& parameters) const KMP_PROFILING(ProfileLevelImportant)
+        {
+            return _bufferCreatorDelegate.CreateBuffer(parameters);
+        }}
+        //--------------------------------------------------------------------------
+
+        bool VulkanBufferManager::CreateBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame /*= false*/) KMP_PROFILING(ProfileLevelImportant)
+        {
+            if (perFrame)
+            {
+                if (_perFrameBuffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("per-frame buffers with sid '{}' have already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _perFrameBuffers.emplace(bufferSid, Array<UPtr<VulkanBuffer>, NumConcurrentFrames>{});
+                if (!hasEmplaced)
+                {
+                    KMP_LOG_ERROR("failed to emplace '{}' per-frame buffers with sid '{}'", NumConcurrentFrames, bufferSid);
+                    return false;
+                }
+
+                for (auto i = 0; i < NumConcurrentFrames; i++)
+                {
+                    _perFrameBuffers[bufferSid][i].reset(_bufferCreatorDelegate.CreateBufferPtr(parameters));
+                }
+
+                return true;
+            }
+            else
+            {
+                if (_buffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("buffer with sid '{}' has already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _buffers.emplace(bufferSid, _bufferCreatorDelegate.CreateBufferPtr(parameters));
+                return hasEmplaced;
+            }
+        }}
+        //--------------------------------------------------------------------------
+
+
+        VulkanVertexBuffer VulkanBufferManager::CreateVertexBuffer(const VulkanBufferParameters& parameters) const KMP_PROFILING(ProfileLevelImportant)
+        {
+            return _bufferCreatorDelegate.CreateVertexBuffer(parameters);
+        }}
+        //--------------------------------------------------------------------------
+
+        bool VulkanBufferManager::CreateVertexBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame /*= false*/) KMP_PROFILING(ProfileLevelImportant)
+        {
+            if (perFrame)
+            {
+                if (_perFrameVertexBuffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("per-frame vertex buffers with sid '{}' have already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _perFrameVertexBuffers.emplace(bufferSid, Array<UPtr<VulkanVertexBuffer>, NumConcurrentFrames>{});
+                if (!hasEmplaced)
+                {
+                    KMP_LOG_ERROR("failed to emplace '{}' per-frame vertex buffers with sid '{}'", NumConcurrentFrames, bufferSid);
+                    return false;
+                }
+
+                for (auto i = 0; i < NumConcurrentFrames; i++)
+                {
+                    _perFrameVertexBuffers[bufferSid][i].reset(_bufferCreatorDelegate.CreateVertexBufferPtr(parameters));
+                }
+
+                return true;
+            }
+            else
+            {
+                if (_vertexBuffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("vertex buffer with sid '{}' has already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _vertexBuffers.emplace(bufferSid, _bufferCreatorDelegate.CreateVertexBufferPtr(parameters));
+                return hasEmplaced;
+            }
+        }}
+        //--------------------------------------------------------------------------
+
+
+        VulkanBuffer VulkanBufferManager::CreateIndexBuffer(const VulkanBufferParameters& parameters) const KMP_PROFILING(ProfileLevelImportant)
+        {
+            return _bufferCreatorDelegate.CreateIndexBuffer(parameters);
+        }}
+        //--------------------------------------------------------------------------
+
+        bool VulkanBufferManager::CreateIndexBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame /*= false*/) KMP_PROFILING(ProfileLevelImportant)
+        {
+            if (perFrame)
+            {
+                if (_perFrameBuffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("per-frame index buffers with sid '{}' have already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _perFrameBuffers.emplace(bufferSid, Array<UPtr<VulkanBuffer>, NumConcurrentFrames>{});
+                if (!hasEmplaced)
+                {
+                    KMP_LOG_ERROR("failed to emplace '{}' per-frame index buffers with sid '{}'", NumConcurrentFrames, bufferSid);
+                    return false;
+                }
+
+                for (auto i = 0; i < NumConcurrentFrames; i++)
+                {
+                    _perFrameBuffers[bufferSid][i].reset(_bufferCreatorDelegate.CreateIndexBufferPtr(parameters));
+                }
+
+                return true;
+            }
+            else
+            {
+                if (_buffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("index buffer with sid '{}' has already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _buffers.emplace(bufferSid, _bufferCreatorDelegate.CreateIndexBufferPtr(parameters));
+                return hasEmplaced;
+            }
+        }}
+        //--------------------------------------------------------------------------
+
+
+        VulkanBuffer VulkanBufferManager::CreateUniformBuffer(const VulkanBufferParameters& parameters) const KMP_PROFILING(ProfileLevelImportant)
+        {
+            return _bufferCreatorDelegate.CreateUniformBuffer(parameters);
+        }}
+        //--------------------------------------------------------------------------
+
+        bool VulkanBufferManager::CreateUniformBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame /*= false*/) KMP_PROFILING(ProfileLevelImportant)
+        {
+            if (perFrame)
+            {
+                if (_perFrameBuffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("per-frame uniform buffers with sid '{}' have already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _perFrameBuffers.emplace(bufferSid, Array<UPtr<VulkanBuffer>, NumConcurrentFrames>{});
+                if (!hasEmplaced)
+                {
+                    KMP_LOG_ERROR("failed to emplace '{}' per-frame uniform buffers with sid '{}'", NumConcurrentFrames, bufferSid);
+                    return false;
+                }
+
+                for (auto i = 0; i < NumConcurrentFrames; i++)
+                {
+                    _perFrameBuffers[bufferSid][i].reset(_bufferCreatorDelegate.CreateUniformBufferPtr(parameters));
+                }
+
+                return true;
+            }
+            else
+            {
+                if (_buffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("uniform buffer with sid '{}' has already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _buffers.emplace(bufferSid, _bufferCreatorDelegate.CreateUniformBufferPtr(parameters));
+                return hasEmplaced;
+            }
+        }}
+        //--------------------------------------------------------------------------
+
+
+        VulkanBuffer VulkanBufferManager::CreateStorageBuffer(const VulkanBufferParameters& parameters) const KMP_PROFILING(ProfileLevelImportant)
+        {
+            return _bufferCreatorDelegate.CreateStorageBuffer(parameters);
+        }}
+        //--------------------------------------------------------------------------
+
+        bool VulkanBufferManager::CreateStorageBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame /*= false*/) KMP_PROFILING(ProfileLevelImportant)
+        {
+            if (perFrame)
+            {
+                if (_perFrameBuffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("per-frame storage buffers with sid '{}' have already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _perFrameBuffers.emplace(bufferSid, Array<UPtr<VulkanBuffer>, NumConcurrentFrames>{});
+                if (!hasEmplaced)
+                {
+                    KMP_LOG_ERROR("failed to emplace '{}' per-frame storage buffers with sid '{}'", NumConcurrentFrames, bufferSid);
+                    return false;
+                }
+
+                for (auto i = 0; i < NumConcurrentFrames; i++)
+                {
+                    _perFrameBuffers[bufferSid][i].reset(_bufferCreatorDelegate.CreateStorageBufferPtr(parameters));
+                }
+
+                return true;
+            }
+            else
+            {
+                if (_buffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("storage buffer with sid '{}' has already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _buffers.emplace(bufferSid, _bufferCreatorDelegate.CreateStorageBufferPtr(parameters));
+                return hasEmplaced;
+            }
+        }}
+        //--------------------------------------------------------------------------
+
+
+        VulkanBuffer VulkanBufferManager::CreateIndirectBuffer(const VulkanBufferParameters& parameters) const KMP_PROFILING(ProfileLevelImportant)
+        {
+            return _bufferCreatorDelegate.CreateIndirectBuffer(parameters);
+        }}
+        //--------------------------------------------------------------------------
+
+        bool VulkanBufferManager::CreateIndirectBufferPtr(StringID bufferSid, const VulkanBufferParameters& parameters, bool perFrame /*= false*/) KMP_PROFILING(ProfileLevelImportant)
+        {
+            if (perFrame)
+            {
+                if (_perFrameBuffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("per-frame indirect buffers with sid '{}' have already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _perFrameBuffers.emplace(bufferSid, Array<UPtr<VulkanBuffer>, NumConcurrentFrames>{});
+                if (!hasEmplaced)
+                {
+                    KMP_LOG_ERROR("failed to emplace '{}' per-frame indirect buffers with sid '{}'", NumConcurrentFrames, bufferSid);
+                    return false;
+                }
+
+                for (auto i = 0; i < NumConcurrentFrames; i++)
+                {
+                    _perFrameBuffers[bufferSid][i].reset(_bufferCreatorDelegate.CreateIndirectBufferPtr(parameters));
+                }
+
+                return true;
+            }
+            else
+            {
+                if (_buffers.contains(bufferSid))
+                {
+                    KMP_LOG_WARN("indirect buffer with sid '{}' has already been created", bufferSid);
+                    return true;
+                }
+
+                const auto [iterator, hasEmplaced] = _buffers.emplace(bufferSid, _bufferCreatorDelegate.CreateIndirectBufferPtr(parameters));
+                return hasEmplaced;
+            }
+        }}
+        //--------------------------------------------------------------------------
+
+
+        Nullable<VulkanBuffer*> VulkanBufferManager::GetBuffer(StringID bufferSid) const noexcept
+        {
+            if (_buffers.contains(bufferSid))
+            {
+                return _buffers.at(bufferSid).get();
+            }
+
+            KMP_LOG_ERROR("failed to find buffer with sid '{}'", bufferSid);
+            return nullptr;
+        }
+        //--------------------------------------------------------------------------
+
+        Nullable<VulkanBuffer*> VulkanBufferManager::GetBuffer(StringID bufferSid, UInt32 index) const noexcept
+        {
+            if (index >= NumConcurrentFrames)
+            {
+                KMP_LOG_ERROR("cannot find per-frame buffer with sid '{}' - index '{}' is out of bounds", bufferSid, index);
+                return nullptr;
+            }
+
+            if (_perFrameBuffers.contains(bufferSid))
+            {
+                return _perFrameBuffers.at(bufferSid)[index].get();
+            }
+
+            KMP_LOG_ERROR("failed to find per-frame buffer with sid '{}'", bufferSid);
+            return nullptr;
+        }
+        //--------------------------------------------------------------------------
+
+        Nullable<VulkanVertexBuffer*> VulkanBufferManager::GetVertexBuffer(StringID bufferSid) const noexcept
+        {
+            if (_vertexBuffers.contains(bufferSid))
+            {
+                return _vertexBuffers.at(bufferSid).get();
+            }
+
+            KMP_LOG_ERROR("failed to find vertex buffer with sid '{}'", bufferSid);
+            return nullptr;
+        }
+        //--------------------------------------------------------------------------
+
+        Nullable<VulkanVertexBuffer*> VulkanBufferManager::GetVertexBuffer(StringID bufferSid, UInt32 index) const noexcept
+        {
+            if (index >= NumConcurrentFrames)
+            {
+                KMP_LOG_ERROR("cannot find per-frame vertex buffer with sid '{}' - index '{}' is out of bounds", bufferSid, index);
+                return nullptr;
+            }
+
+            if (_perFrameVertexBuffers.contains(bufferSid))
+            {
+                return _perFrameVertexBuffers.at(bufferSid)[index].get();
+            }
+
+            KMP_LOG_ERROR("failed to find per-frame vertex buffer with sid '{}'", bufferSid);
+            return nullptr;
         }
         //--------------------------------------------------------------------------
     }
