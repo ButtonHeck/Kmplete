26624afacd988cc04e869a005d4bde946ec8457c
Ilya Malgin <Soundmaker1505@gmail.com>
2026-07-10 09:41:46 +0300

    ShaderCompilerTool comments added

--- src/Tools/ShaderCompiler/shader_compiler_tool.h
+++ src/Tools/ShaderCompiler/shader_compiler_tool.h
@@ -34,7 +34,30 @@ namespace Kmplete
             //--------------------------------------------------------------------------
 
 
-            //TODO: comments
+            //! The processor class for compiling shaders from source files to SPIRV binary. Supports two work modes:
+            //! 1) Direct mode - pass shaders source file paths, SPIRV destination file paths and shader types
+            //! directly as arguments lists. In this mode all three lists' lengths should match each other in order
+            //! to work correctly. e.g.:
+            //! --mode Direct --source_files "file1.vert" "file2.frag" --destination-files "outFile1.vert.spv" "outFile2.frag.spv" --shader_types "Vertex" "Fragment"
+            //!                               1            2                                1                   2                                  1        2
+            //! 2) Json mode - pass all the parameters for shaders compilation in json file(s) each of which is in the following format:
+            //! {
+            //!     "Shaders": [
+            //!         {
+            //!             "Source": "path_to_shader1.vert",
+            //!             "Destination": "path_to_shader1_output.vert.spv",
+            //!             "ShaderType": "Vertex"
+            //!         },
+            //!         {
+            //!             "Source": "path_to_shader2.frag",
+            //!             "Destination": "path_to_shader2_output.frag.spv",
+            //!             "ShaderType": "Fragment"
+            //!         }
+            //!     ]
+            //! }
+            //! 
+            //! e.g:
+            //! --mode Json --source_files "path_to_shaders1.json" "path_to_shaders2.json"
             class ShaderCompilerProcessor
             {
                 KMP_LOG_CLASSNAME(ShaderCompilerProcessor)
